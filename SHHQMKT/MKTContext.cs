1df363f6cdf47c38ca5ba231a2d7579f43f7168a
Cris-Li <l_crystalmei@163.com>
2019-03-27 09:54:26 +0800

    添加项目文件。

---
+++ SHHQMKT/MKTContext.cs
@@ -0,0 +1,129 @@
+﻿namespace SHHQMKT
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class MKTContext : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“MKTContext”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“SHHQMKT.MKTContext”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“MKTContext”
+        //连接字符串。
+        public MKTContext()
+            : base("name=MKTContext")
+        {
+            Database.SetInitializer<MKTContext>(new DropCreateDatabaseIfModelChanges<MKTContext>());
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public DbSet<MKTDT> MKTDTs { get; set; }
+        public DbSet<CPXXMMDD> CPXXMMDDs { get; set; }
+    }
+
+    public class MKTDT
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(4)]
+        public string MDStreamID { get; set; }
+        [MaxLength(6)]
+        public string SecurityID { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+        public decimal TradeVolume { get; set; }
+        public decimal TotalValueTraded { get; set; }
+        public decimal PreClosePx { get; set; }
+        public decimal OpenPrice { get; set; }
+        public decimal HighPrice { get; set; }
+        public decimal LowPrice { get; set; }
+        public decimal TradePrice { get; set; }
+        public decimal ClosePx { get; set; }
+        public decimal BuyPrice1 { get; set; }
+        public decimal BuyVolume1 { get; set; }
+        public decimal SellPrice1 { get; set; }
+        public decimal SellVolume1 { get; set; }
+        public decimal BuyPrice2 { get; set; }
+        public decimal BuyVolume2 { get; set; }
+        public decimal SellPrice2 { get; set; }
+        public decimal SellVolume2 { get; set; }
+        public decimal BuyPrice3 { get; set; }
+        public decimal BuyVolume3 { get; set; }
+        public decimal SellPrice3 { get; set; }
+        public decimal SellVolume3 { get; set; }
+        public decimal BuyPrice4 { get; set; }
+        public decimal BuyVolume4 { get; set; }
+        public decimal SellPrice4 { get; set; }
+        public decimal SellVolume4 { get; set; }
+        public decimal BuyPrice5 { get; set; }
+        public decimal BuyVolume5 { get; set; }
+        public decimal SellPrice5 { get; set; }
+        public decimal SellVolume5 { get; set; }
+        public decimal PreCloseIOPV { get; set; }
+        public decimal IOPV { get; set; }
+        public string TradingPhaseCode { get; set; }
+        public string Timestamp { get; set; }
+    }
+
+    public class CPXXMMDD
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(6)]
+        public string SecurityID { get; set; }
+        [MaxLength(12)]
+        public string ISIN { get; set; }
+        [MaxLength(8)]
+        public string Updatetime { get; set; }
+        [MaxLength(8)]
+        public string ChineseName { get; set; }
+        [MaxLength(10)]
+        public string EnglishName { get; set; }
+        [MaxLength(6)]
+        public string BasicCode { get; set; }
+        [MaxLength(4)]
+        public string MarketType { get; set; }
+        [MaxLength(6)]
+        public string SecurityType { get; set; }
+        [MaxLength(3)]
+        public string SecuritySubType { get; set; }
+        [MaxLength(3)]
+        public string CurrencyType { get; set; }
+        public decimal FaceValue { get; set; }
+        public decimal UnmarketableAmount { get; set; }
+        [MaxLength(15)]
+        public string LastTradeDate { get; set; }
+        [MaxLength(8)]
+        public string IssueDate { get; set; }
+        [MaxLength(3)]
+        public string SETNo { get; set; }
+        public decimal BuyUnit { get; set; }
+        public decimal SellUnit { get; set; }
+        public decimal AmountLowLimit { get; set; }
+        public decimal AmountHighLimt { get; set; }
+        public decimal PreClose { get; set; }
+        public decimal PriceStall { get; set; }
+        [MaxLength(1)]
+        public string PriceLimitType { get; set; }
+        public decimal HighLimt { get; set; }
+        public decimal LowLimit_ { get; set; }
+        public decimal ExRightsRatio { get; set; }
+        public decimal ExDividendAmount { get; set; }
+        [MaxLength(1)]
+        public string FinancingTag { get; set; }
+        [MaxLength(1)]
+        public string MarginTag { get; set; }
+        [MaxLength(20)]
+        public string State { get; set; }
+        [MaxLength(50)]
+        public string Comment { get; set; }
+    }
+
+
+}
---
+++ SHHQMKT/Program.cs
@@ -0,0 +1,135 @@
+﻿using EntityFramework.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHHQMKT
+{
+    class Program
+    {
+        static public MKTDT MKTDTParse(string[] strArr)
+        {
+            if (strArr[0] == "MD003")
+            {
+                return new MKTDT()
+                {
+                    MDStreamID = strArr[0],
+                    SecurityID = strArr[1],
+                    Symbol = strArr[2],
+                    TradeVolume = decimal.Parse(strArr[3]),
+                    TotalValueTraded = decimal.Parse(strArr[4]),
+                    PreClosePx = decimal.Parse(strArr[5]),
+                    OpenPrice = decimal.Parse(strArr[6]),
+                    HighPrice = decimal.Parse(strArr[7]),
+                    LowPrice = decimal.Parse(strArr[8]),
+                    TradePrice = decimal.Parse(strArr[9]),
+                    ClosePx = decimal.Parse(strArr[10]),
+                    BuyPrice1 = decimal.Parse(strArr[11]),
+                    BuyVolume1 = decimal.Parse(strArr[12]),
+                    SellPrice1 = decimal.Parse(strArr[13]),
+                    SellVolume1 = decimal.Parse(strArr[14]),
+                    BuyPrice2 = decimal.Parse(strArr[15]),
+                    BuyVolume2 = decimal.Parse(strArr[16]),
+                    SellPrice2 = decimal.Parse(strArr[17]),
+                    SellVolume2 = decimal.Parse(strArr[18]),
+                    BuyPrice3 = decimal.Parse(strArr[19]),
+                    BuyVolume3 = decimal.Parse(strArr[20]),
+                    SellPrice3 = decimal.Parse(strArr[21]),
+                    SellVolume3 = decimal.Parse(strArr[22]),
+                    BuyPrice4 = decimal.Parse(strArr[23]),
+                    BuyVolume4 = decimal.Parse(strArr[24]),
+                    SellPrice4 = decimal.Parse(strArr[25]),
+                    SellVolume4 = decimal.Parse(strArr[26]),
+                    BuyPrice5 = decimal.Parse(strArr[27]),
+                    BuyVolume5 = decimal.Parse(strArr[28]),
+                    SellPrice5 = decimal.Parse(strArr[29]),
+                    SellVolume5 = decimal.Parse(strArr[30]),
+                    TradingPhaseCode = strArr[31],
+                    Timestamp = strArr[32]
+                };
+            }
+            else if (strArr[0] == "MD004")
+            {
+                return new MKTDT()
+                {
+                    MDStreamID = strArr[0],
+                    SecurityID = strArr[1],
+                    Symbol = strArr[2],
+                    TradeVolume = decimal.Parse(strArr[3]),
+                    TotalValueTraded = decimal.Parse(strArr[4]),
+                    PreClosePx = decimal.Parse(strArr[5]),
+                    OpenPrice = decimal.Parse(strArr[6]),
+                    HighPrice = decimal.Parse(strArr[7]),
+                    LowPrice = decimal.Parse(strArr[8]),
+                    TradePrice = decimal.Parse(strArr[9]),
+                    ClosePx = decimal.Parse(strArr[10]),
+                    BuyPrice1 = decimal.Parse(strArr[11]),
+                    BuyVolume1 = decimal.Parse(strArr[12]),
+                    SellPrice1 = decimal.Parse(strArr[13]),
+                    SellVolume1 = decimal.Parse(strArr[14]),
+                    BuyPrice2 = decimal.Parse(strArr[15]),
+                    BuyVolume2 = decimal.Parse(strArr[16]),
+                    SellPrice2 = decimal.Parse(strArr[17]),
+                    SellVolume2 = decimal.Parse(strArr[18]),
+                    BuyPrice3 = decimal.Parse(strArr[19]),
+                    BuyVolume3 = decimal.Parse(strArr[20]),
+                    SellPrice3 = decimal.Parse(strArr[21]),
+                    SellVolume3 = decimal.Parse(strArr[22]),
+                    BuyPrice4 = decimal.Parse(strArr[23]),
+                    BuyVolume4 = decimal.Parse(strArr[24]),
+                    SellPrice4 = decimal.Parse(strArr[25]),
+                    SellVolume4 = decimal.Parse(strArr[26]),
+                    BuyPrice5 = decimal.Parse(strArr[27]),
+                    BuyVolume5 = decimal.Parse(strArr[28]),
+                    SellPrice5 = decimal.Parse(strArr[29]),
+                    SellVolume5 = decimal.Parse(strArr[30]),
+                    PreCloseIOPV = decimal.Parse(strArr[31]),
+                    IOPV = decimal.Parse(strArr[32]),
+                    TradingPhaseCode = strArr[33],
+                    Timestamp = strArr[34]
+                };
+            }
+            else return null;
+
+        }
+
+        static public void JobMKT(string strReadFilePath)
+        {
+            MKTContext db = new MKTContext();
+
+            StreamReader srReadFile = new StreamReader(strReadFilePath);
+            List<MKTDT> mKTDT00s = new List<MKTDT>();
+            //循环读入
+            while (!srReadFile.EndOfStream)
+            {
+                string strReadLine = srReadFile.ReadLine(); //读取每行数据
+                string[] strArr = strReadLine.Replace(" ", "").Split('|');
+                MKTDT mkt = MKTDTParse(strArr);
+                if (mkt != null)
+                {
+                    mKTDT00s.Add(mkt);
+                    //db.MKTDT00s.Where(c => c.SecurityID == mkt.SecurityID).UpdateAsync(c => new MKTDT00 { OpenPrice = 100 });
+                }
+                Console.WriteLine(strReadLine); //屏幕打印每行数据
+            }
+            db.SaveChanges();
+            // 关闭读取流文件
+            srReadFile.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            //JobMKT("C:\\Users\\l_cry\\Desktop\\MKTDT00.TXT");
+            using (var ctx = new MKTContext())
+            {
+                var stud = new MKTDT() { SecurityID = "000300" };
+
+                ctx.MKTDTs.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
